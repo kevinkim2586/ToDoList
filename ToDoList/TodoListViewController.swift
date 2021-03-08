9c0e1c1ce1f5ffa1e6524ef1876b0be61432e9e7
김영채 <kevinkim2586@gmail.com>
2021-03-08 16:47:11 +0900

    Add tableview

--- ToDoList/TodoListViewController.swift
+++ ToDoList/TodoListViewController.swift
@@ -1,6 +1,8 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggs", "Destroy wonderland"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -10,3 +12,22 @@ class TodoListViewController: UITableViewController {
 
 }
 
+extension TodoListViewController {
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        
+        return cell
+    }
+    
+    
+}
