2e48e067906e3130d2590b7df1497dd28029cc06
김영채 <kevinkim2586@gmail.com>
2021-03-15 15:20:54 +0900

    Implement MVC Design Pattern
    - Separated Data Model for each cell as a class

--- ToDoList/Controller/TodoListViewController.swift
+++ ToDoList/Controller/TodoListViewController.swift
@@ -2,16 +2,31 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = ["Find Mike", "Buy Eggs", "Destroy wonderland"]
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
-        }
+        
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Buy eggs"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Destroy"
+        itemArray.append(newItem3)
+  
+        
+        
+//        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+//            itemArray = items
+//        }
   
     }
 
@@ -28,7 +43,12 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             
             // What will happen once the user clicks the Add Item button on our UIAlert
-            self.itemArray.append(textField.text!)
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            
+            self.itemArray.append(newItem)
             
             
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
@@ -65,7 +85,13 @@ extension TodoListViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        
+        if itemArray[indexPath.row].isDone == true {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
         
         
         return cell
@@ -75,18 +101,18 @@ extension TodoListViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
+        if itemArray[indexPath.row].isDone == false {
+            itemArray[indexPath.row].isDone = true
+        } else {
+            itemArray[indexPath.row].isDone = false
+        }
+        
         guard let cell = tableView.cellForRow(at: indexPath) else {
             return
         }
         
-        if cell.accessoryType != .checkmark {
-            cell.accessoryType = .checkmark
-        } else {
-            cell.accessoryType = .none
-        }
-        
-     
-
+        tableView.reloadData()
+    
         tableView.deselectRow(at: indexPath, animated: true)    // cell 을 누르자마자 deselect 되는 애니메이션이 나올 수 있도록
         
     }
---
+++ ToDoList/Data Model/Item.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var isDone: Bool = false
+    
+}
