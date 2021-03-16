594d8dc059dcc2889543d6309bd3181814f2f549
김영채 <kevinkim2586@gmail.com>
2021-03-16 17:34:40 +0900

    Implement UISearchBar and query with Core Data

--- ToDoList/Controller/TodoListViewController.swift
+++ ToDoList/Controller/TodoListViewController.swift
@@ -3,15 +3,17 @@ import CoreData
 
 class TodoListViewController: UITableViewController {
     
+    @IBOutlet weak var searchBar: UISearchBar!
+
     var itemArray = [Item]()
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
-  
-        
-        
+    
+        searchBar.delegate = self
+
         print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         loadItems()
     }
@@ -20,7 +22,7 @@ class TodoListViewController: UITableViewController {
         
         var textField = UITextField()
         
-        let alert = UIAlertController(title: "Add New ToDo Item", message: "", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Add New To Do Item", message: "", preferredStyle: .alert)
         
         // The button you are going to press once you are done
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
@@ -30,7 +32,6 @@ class TodoListViewController: UITableViewController {
             let newItem = Item(context: self.context)
             newItem.title = textField.text!
             newItem.isDone = false
-            
             
             self.itemArray.append(newItem)
             self.saveItems()
@@ -69,6 +70,7 @@ class TodoListViewController: UITableViewController {
         } catch {
             print("Error fetching data from context \(error)")
         }
+        tableView.reloadData()
     }
     
 
@@ -89,7 +91,6 @@ extension TodoListViewController {
         let item = itemArray[indexPath.row]
         
         cell.textLabel?.text = item.title
-        
         cell.accessoryType = item.isDone ? .checkmark : .none
 
         return cell
@@ -101,14 +102,43 @@ extension TodoListViewController {
         
         itemArray[indexPath.row].isDone = !itemArray[indexPath.row].isDone
         saveItems()
-        
-        guard let cell = tableView.cellForRow(at: indexPath) else {
-            return
-        }
     
         tableView.deselectRow(at: indexPath, animated: true)    // cell 을 누르자마자 deselect 되는 애니메이션이 나올 수 있도록
-        
     }
+}
 
+//MARK: - UISearchBarDelegate
+
+extension TodoListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        
+        
+        // What do we want to get back from the database? Use NSPredicate
+        // NSPredicate is basically a foundation class that specifies how data should be fetched/filtered. (query language)
+        // %@ substitutes any sort of argument that you want to pass in
+        // [cd] making it "case" and "diacritic" insensitive
+         
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        let sortDescriptor = NSSortDescriptor(key: "title", ascending: true)
+        
+        // Why array? because .sortDescriptor"s" -> plural -> expects an array
+        request.sortDescriptors = [sortDescriptor]
+
+        
+        do {
+           itemArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+        tableView.reloadData()
+
+
+    }
+    
+    
     
 }
