665fae5345dddaa15d719f9e5caedf7a9dd96b43
김영채 <kevinkim2586@gmail.com>
2021-03-17 14:21:13 +0900

    Add Constants file

---
+++ ToDoList/Constants.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+struct Constants {
+    
+    // Cell identifiers
+    static let categoryCellIdentifier = "CategoryCell"
+    static let toDoItemCellIdentifier = "ToDoItemCell"
+    
+
+    // Segue identifiers
+    static let goToItemSegue = "goToItems"
+}
