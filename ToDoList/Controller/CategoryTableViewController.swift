bc7822b85f7b847e931c4d6397dd5cb00d838a4c
김영채 <kevinkim2586@gmail.com>
2021-03-17 14:03:37 +0900

    Setup relational database for Core Data

--- ToDoList/Controller/CategoryTableViewController.swift
+++ ToDoList/Controller/CategoryTableViewController.swift
@@ -7,19 +7,6 @@ class CategoryTableViewController: UITableViewController {
 
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
-        return 0
-    }
-    
-    
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
         
